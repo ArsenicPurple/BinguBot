e6b683c1d64cee1b3a8cfc0c4e92fc4566367411
ArsenicPurple <56276124+ArsenicPurple@users.noreply.github.com>
2021-11-07 14:54:17 -0500

    For Work

---
+++ BinguBot/Database.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace BinguBot
+{
+    class Database
+    {
+        public static Database Instance = new Database();
+        public Database()
+        {
+            string connectionString;
+            SqlConnection conn;
+
+            connectionString = @"Data Source=DESKTOP-K5TIM4N;Initial Catalog=BinguBot;User ID=samcr;Password=Fatesept9=";
+
+            conn = new SqlConnection(connectionString);
+
+            conn.Open();
+            Console.WriteLine("");
+        }
+
+    }
+}
